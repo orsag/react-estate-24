aaefee12d601c2f085144ee2a84d91ef0172bd18
Martin Orsag <martin.orsag@websupport.eu>
2024-05-21 11:17:04 +0200

    Finnished project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
-import './layout.scss';
+import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
+import './index.scss';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        // {
+        //   path:"/profile",
+        //   element:<ProfilePage/>
+        // },
+        // {
+        //   path:"/login",
+        //   element:<Login/>
+        // },
+        // {
+        //   path:"/register",
+        //   element:<Register/>
+        // }
+      ]
+    }
+  ]);
+
   return (
-    <div className="layout">Hello World</div>
-  )
+    <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
